2bf5c224ee0d5a3934c829b8e8e763edd10d1675
Johan <johan@amberitservices.com.au>
2022-08-27 21:31:03 +1000

    Additional error checking added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 import csv
 from var_dump import var_dump
-
+import logging
 app = Flask(__name__)
 
 
@@ -13,19 +13,38 @@ def show_index():
     image_folder = os.path.join('static', 'img')
     app.config['IMAGE_FOLDER'] = image_folder
     ttc_logo = os.path.join(app.config['IMAGE_FOLDER'], 'logo_ttc.png')
-    site_data = {'logo_image': ttc_logo}
+    if os.path.exists(ttc_logo):
+        site_data = {'logo_image': ttc_logo}
+    else:
+        site_data = {'logo_image': ""}
+        logging.warning('Logo image is not found where expected.')
     today = datetime.today()
     report_date = today.strftime("%d/%m/%Y %H:%M:%S")
     site_data['report_date'] = report_date
     data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'addedusers.csv')
-    addedusers = read_files(data_file_path, "add")
+    if os.path.exists(data_file_path):
+        addedusers = read_files(data_file_path, "add")
+    else:
+        addedusers = []
+        logging.info('No Users where added')
     data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'deletedusers.csv')
-    deletedusers = read_files(data_file_path)
+    if os.path.exists(data_file_path):
+        deletedusers = read_files(data_file_path)
+    else:
+        deletedusers = []
+        logging.info('No Users where deleted')
     data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'incorrect_users.csv')
-    incorrectusers = read_files(data_file_path)
-    var_dump(incorrectusers)
+    if os.path.exists(data_file_path):
+        incorrectusers = read_files(data_file_path)
+    else:
+        incorrectusers = []
+        logging.info('No incorrect Users where found')
     data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'full_user_list.csv')
-    allusers = read_files(data_file_path)
+    if os.path.exists(data_file_path):
+        allusers = read_files(data_file_path)
+    else:
+        allusers = []
+        logging.warning('No All Users File Found!')
     return render_template("index.html", site_data=site_data, addedusers=addedusers, deletedusers=deletedusers,
                            allusers=allusers, incorrectusers=incorrectusers)
 
