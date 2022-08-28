903af44a366e77afbeb35b38b22724fdcdf88840
Johan <johan@amberitservices.com.au>
2022-08-28 22:26:19 +1000

    Moved files to common location. Updated report look.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from datetime import datetime
 import os
 import csv
-from var_dump import var_dump
+# from var_dump import var_dump
 import logging
 app = Flask(__name__)
 
@@ -21,25 +21,29 @@ def show_index():
     today = datetime.today()
     report_date = today.strftime("%d/%m/%Y %H:%M:%S")
     site_data['report_date'] = report_date
-    data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'addedusers.csv')
+    data_file_path = "c:\\files\\addedusers.csv"
+# data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'addedusers.csv')
     if os.path.exists(data_file_path):
         addedusers = read_files(data_file_path, "add")
     else:
         addedusers = []
         logging.info('No Users where added')
-    data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'deletedusers.csv')
+    data_file_path = "c:\\files\\deletedusers.csv"
+# data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'deletedusers.csv')
     if os.path.exists(data_file_path):
         deletedusers = read_files(data_file_path)
     else:
         deletedusers = []
         logging.info('No Users where deleted')
-    data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'incorrect_users.csv')
+    data_file_path = "c:\\files\\incorrect_users.csv"
+    # data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'incorrect_users.csv')
     if os.path.exists(data_file_path):
         incorrectusers = read_files(data_file_path)
     else:
         incorrectusers = []
         logging.info('No incorrect Users where found')
-    data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'full_user_list.csv')
+    data_file_path = "c:\\files\\full_user_list.csv"
+    # data_file_path = os.path.join(os.path.dirname(__file__), 'static\\files', 'full_user_list.csv')
     if os.path.exists(data_file_path):
         allusers = read_files(data_file_path)
     else:
